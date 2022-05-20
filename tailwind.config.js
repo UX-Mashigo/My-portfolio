a51731dff17f4d289b3ebfe385e07f1b211542cd
Unite Mashigo <unite.mashigo@dadevs.co.za>
2022-05-20 16:18:24 +0200

    Portfolio header

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        green: "#9AD8A2",
+        blue: "#02ADA6",
+        yellow: "#F9D972",
+        grey: "#f4f4f4",
+        red: "#af203a",
+        brown: "#111111",
+        
+        
+      },
+    },
+  },
+  plugins: [],
+}
